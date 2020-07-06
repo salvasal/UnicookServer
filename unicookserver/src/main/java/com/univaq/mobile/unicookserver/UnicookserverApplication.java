c10242b46663e8818e9a4f55660e22ee1fabbb3c
Salvatore <s.salernitano@gp-start.it>
2020-07-06 21:33:46 +0200

    Create SpringBoot Unicookserver

---
+++ unicookserver/src/main/java/com/univaq/mobile/unicookserver/UnicookserverApplication.java
@@ -0,0 +1,13 @@
+package com.univaq.mobile.unicookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicookserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicookserverApplication.class, args);
+    }
+
+}
